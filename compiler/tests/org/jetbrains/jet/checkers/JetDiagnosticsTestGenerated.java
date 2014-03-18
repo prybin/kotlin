95ede926ddacdfd8090c1007217632de5d422626
Svetlana Isakova <svetlana.isakova@jetbrains.com>
2014-03-18 15:02:53 +0400

    Improved error reporting for invoke on expression

--- compiler/tests/org/jetbrains/jet/checkers/JetDiagnosticsTestGenerated.java
+++ compiler/tests/org/jetbrains/jet/checkers/JetDiagnosticsTestGenerated.java
@@ -6060,6 +6060,11 @@ public class JetDiagnosticsTestGenerated extends AbstractJetDiagnosticsTest {
                         doTest("compiler/testData/diagnostics/tests/resolve/invoke/errors/invisibleInvoke.kt");
                     }
                     
+                    @TestMetadata("receiverPresenceErrorForInvoke.kt")
+                    public void testReceiverPresenceErrorForInvoke() throws Exception {
+                        doTest("compiler/testData/diagnostics/tests/resolve/invoke/errors/receiverPresenceErrorForInvoke.kt");
+                    }
+                    
                     @TestMetadata("typeInferenceErrorForInvoke.kt")
                     public void testTypeInferenceErrorForInvoke() throws Exception {
                         doTest("compiler/testData/diagnostics/tests/resolve/invoke/errors/typeInferenceErrorForInvoke.kt");
@@ -6073,6 +6078,11 @@ public class JetDiagnosticsTestGenerated extends AbstractJetDiagnosticsTest {
                     @TestMetadata("unsafeCallWithInvoke.kt")
                     public void testUnsafeCallWithInvoke() throws Exception {
                         doTest("compiler/testData/diagnostics/tests/resolve/invoke/errors/unsafeCallWithInvoke.kt");
+                    }
+                    
+                    @TestMetadata("wrongReceiverForInvokeOnExpression.kt")
+                    public void testWrongReceiverForInvokeOnExpression() throws Exception {
+                        doTest("compiler/testData/diagnostics/tests/resolve/invoke/errors/wrongReceiverForInvokeOnExpression.kt");
                     }
                     
                     @TestMetadata("wrongReceiverTypeForInvoke.kt")

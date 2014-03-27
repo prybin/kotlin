3c702e9b3ac693cf2f2641d3301073dc4a7e70dd
Pavel V. Talanov <talanov.pavel@gmail.com>
2014-03-27 19:58:14 +0400

    JetPlaceHolderStubElementType: move boilerplate removed

--- compiler/frontend/src/org/jetbrains/jet/lang/psi/stubs/elements/JetPlaceHolderStubElementType.java
+++ compiler/frontend/src/org/jetbrains/jet/lang/psi/stubs/elements/JetPlaceHolderStubElementType.java
@@ -16,7 +16,12 @@
 
 package org.jetbrains.jet.lang.psi.stubs.elements;
 
-import com.intellij.psi.stubs.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.stubs.IndexSink;
+import com.intellij.psi.stubs.StubElement;
+import com.intellij.psi.stubs.StubInputStream;
+import com.intellij.psi.stubs.StubOutputStream;
+import com.intellij.util.ReflectionUtil;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jet.lang.psi.JetElement;
@@ -24,10 +29,34 @@ import org.jetbrains.jet.lang.psi.stubs.PsiJetPlaceHolderStub;
 import org.jetbrains.jet.lang.psi.stubs.impl.PsiJetPlaceHolderStubImpl;
 
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 
 public abstract class JetPlaceHolderStubElementType<T extends JetElement> extends JetStubElementType<PsiJetPlaceHolderStub<T>, T> {
-    public JetPlaceHolderStubElementType(@NotNull @NonNls String debugName) {
+
+    @NotNull
+    private final Constructor<T> byNodeConstructor;
+    @NotNull
+    private final Constructor<T> byStubConstructor;
+
+    public JetPlaceHolderStubElementType(@NotNull @NonNls String debugName, @NotNull Class<T> clazz) {
         super(debugName);
+        try {
+            byNodeConstructor = clazz.getConstructor(ASTNode.class);
+            byStubConstructor = clazz.getConstructor(PsiJetPlaceHolderStub.class);
+        }
+        catch (NoSuchMethodException e) {
+            throw new RuntimeException("Stub element type declaration for " + clazz.getSimpleName() + " is missing required constructors",e);
+        }
+    }
+
+    @Override
+    public T createPsiFromAst(@NotNull ASTNode node) {
+        return ReflectionUtil.createInstance(byNodeConstructor, node);
+    }
+
+    @Override
+    public T createPsi(@NotNull PsiJetPlaceHolderStub<T> stub) {
+        return ReflectionUtil.createInstance(byStubConstructor, stub);
     }
 
     @Override

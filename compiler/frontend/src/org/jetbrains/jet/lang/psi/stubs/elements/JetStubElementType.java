b875207701e6b82cf6d8496eb9cdd8201cbe8479
Pavel V. Talanov <talanov.pavel@gmail.com>
2014-04-03 16:02:40 +0400

    EARLY: improve base stubs

--- compiler/frontend/src/org/jetbrains/jet/lang/psi/stubs/elements/JetStubElementType.java
+++ compiler/frontend/src/org/jetbrains/jet/lang/psi/stubs/elements/JetStubElementType.java
@@ -19,6 +19,7 @@ package org.jetbrains.jet.lang.psi.stubs.elements;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.stubs.IStubElementType;
+import com.intellij.psi.stubs.IndexSink;
 import com.intellij.psi.stubs.StubElement;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.ReflectionUtil;
@@ -100,5 +101,10 @@ public abstract class JetStubElementType<StubT extends StubElement, PsiT extends
         }
 
         return super.shouldCreateStub(node);
+    }
+
+    @Override
+    public void indexStub(@NotNull StubT stub, @NotNull IndexSink sink) {
+        // do not force inheritors to implement this method
     }
 }
